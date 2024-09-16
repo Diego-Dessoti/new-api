9d897d05a74223633f97d7411c3cba060db5fa51
diego-dessoti <didessoti12345@gmail.com>
2024-09-16 05:26:27 -0300

    New project

---
+++ mongodbWithSpring/src/main/java/com/dessotidiego/mongodbWithSpring/MongodbWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dessotidiego.mongodbWithSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbWithSpringApplication.class, args);
+	}
+
+}
